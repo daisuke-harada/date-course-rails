74b5234b26d1c1e1f7400b2145d8942b3c047d28
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-11 12:04:50 +0900

    refactor: AddressSerializer を DateSpotSerializer にフラット統合し date_spot ネストを廃止

--- app/controllers/api/v1/prefectures_controller.rb
+++ app/controllers/api/v1/prefectures_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PrefecturesController < ApplicationController
   def show
     address_and_date_spots = DateSpot.includes(:date_spot_reviews).where(prefecture_id: params[:id]).map do |date_spot|
-      AddressSerializer.new(date_spot).serializable_hash
+      DateSpotSerializer.new(date_spot).serializable_hash
     end
 
     render status: :ok, json: address_and_date_spots
--- app/serializers/date_spot_serializer.rb
+++ app/serializers/date_spot_serializer.rb
@@ -1,36 +1,25 @@
-# == Schema Information
-#
-# Table name: date_spots
-#
-#  id            :bigint           not null, primary key
-#  city_name     :string           not null
-#  closing_time  :datetime
-#  image         :string
-#  latitude      :float
-#  longitude     :float
-#  name          :string           not null
-#  opening_time  :datetime
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  genre_id      :integer
-#  prefecture_id :integer
-#
-# Indexes
-#
-#  index_date_spots_on_genre_id_and_created_at       (genre_id,created_at)
-#  index_date_spots_on_prefecture_id_and_created_at  (prefecture_id,created_at)
-#
 class DateSpotSerializer < ActiveModel::Serializer
-  has_many :date_spot_reviews
-  has_many :courses
+  attributes :id, :name, :genre_id, :opening_time, :closing_time,
+             :created_at, :updated_at,
+             :city_name, :latitude, :longitude
 
-  attributes :id, :name, :image, :closing_time, :opening_time, :created_at, :updated_at
+  attribute :image do
+    { url: object.image.url }
+  end
+
+  attribute :prefecture_name do
+    object.prefecture_name
+  end
+
+  attribute :genre_name do
+    object.genre.name
+  end
+
+  attribute :review_total_number do
+    object.date_spot_reviews.size
+  end
 
   attribute :average_rate do
     object.average_rate_calculation
-  end
-
-  attribute :genre_id do
-    object.genre_id
   end
 end
