0610dbd07d8ca3ea92e24a3d1723c00a7b8bb282
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-23 12:17:09 +0900

    test: users update/destroy に JWT 認証テストを追加 (#439)

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe "Api::V1::Users", type: :request do
         "gender" => "女性",
         "password" => "edit_password",
         "password_confirmation" => "edit_password"
-      }
+      }, headers: auth_headers(user)
       expect(response.status).to eq(200)
       expect(JSON.parse(response.body)["name"]).to eq("edit")
       expect(JSON.parse(response.body)["email"]).to eq("edit@gmail.com")
@@ -44,7 +44,7 @@ RSpec.describe "Api::V1::Users", type: :request do
         "gender" => "",
         "password" => "",
         "password_confirmation" => ""
-      }
+      }, headers: auth_headers(user)
       expect(response.status).to eq(422)
       expect(JSON.parse(response.body)["error_messages"]["name"]).to eq(["を入力してください"])
       expect(JSON.parse(response.body)["error_messages"]["email"]).to eq(["を入力してください", "は不正な値です"])
@@ -52,12 +52,22 @@ RSpec.describe "Api::V1::Users", type: :request do
       expect(JSON.parse(response.body)["error_messages"]["password"]).to eq(nil)
       expect(JSON.parse(response.body)["error_messages"]["password_confirmation"]).to eq(nil)
     end
+
+    it "未認証の場合は 401 を返す" do
+      put "/api/v1/users/#{user.id}", params: {"name" => "edit"}
+      expect(response.status).to eq(401)
+    end
   end
 
   describe "DELETE /destroy" do
     it "user情報の削除に成功する" do
-      delete "/api/v1/users/#{user.id}"
+      delete "/api/v1/users/#{user.id}", headers: auth_headers(user)
       expect(response.status).to eq(204)
+    end
+
+    it "未認証の場合は 401 を返す" do
+      delete "/api/v1/users/#{user.id}"
+      expect(response.status).to eq(401)
     end
   end
 end
