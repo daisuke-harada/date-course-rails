f08e42991c759bbe229100e180655baf40612aa9
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-30 00:31:14 +0900

    fix: top response

--- app/controllers/api/v1/homes_controller.rb
+++ app/controllers/api/v1/homes_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::HomesController < ApplicationController
   def top
     render status: :ok, json: {
-      areas: Area.all,
-      main_prefectures: Prefecture.majors,
-      main_genres: Genre.majors,
-      genres: Genre.all,
-      date_spots: DateSpot.includes(:date_spot_reviews).all.map { |date_spot| DateSpotSerializer.new(date_spot).as_json }
+      areas: Area.all.map { |a| {id: a.id, name: a.name} },
+      main_prefectures: Prefecture.majors.map { |p| {id: p.id, name: p.name} },
+      main_genres: Genre.majors.map { |g| {id: g.id, name: g.name} },
+      genres: Genre.all.map { |g| {id: g.id, name: g.name} },
+      date_spots: DateSpot.includes(:date_spot_reviews).all.map { |date_spot| DateSpotSerializer.new(date_spot).serializable_hash }
     }
   end
 end
